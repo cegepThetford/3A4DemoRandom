e93ba83539e6915742026e8164d9ad950104a6bf
Mathieu Morissette <mmorissette2@cegepthetford.ca>
2024-02-16 10:48:45 -0500

    Ajoutez des fichiers projet.

---
+++ DemoRandom_Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoRandom_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand1 = new Random();
+            Random rand2 = new Random();
+            Thread.Sleep(20);
+            Random rand3 = new Random();
+            afficherSerieNombres(rand1);
+            afficherSerieNombres(rand2);
+            afficherSerieNombres(rand3);
+            Console.ReadLine();
+        }
+
+        private static void afficherSerieNombres(Random rand)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+                Console.Write("{0,5}", rand.Next(1, 100));
+            Console.WriteLine();
+        }
+    }
+}
